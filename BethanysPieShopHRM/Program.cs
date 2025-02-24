24251bf8b26c2784c45965de2fe37e84818d98c6
Konradovsky <konrad.trade@gmail.com>
2025-02-24 07:53:00 +0100

    feat: first commit

---
+++ BethanysPieShopHRM/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using BethanysPieShopHRM;
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+
+string employee = "Employee";
+
+bethany.DisplayEmployeeDetails();
+
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork(5);
+bethany.PerformWork();
+
+double receivedWageBethany = bethany.ReceiveWage(ref employee, true );
+Console.WriteLine($"Wage paid (message from Program): {receivedWageBethany}");
+
+bethany.firstName = "John";
+bethany.hourlyRate = 10;
+
+bethany.DisplayEmployeeDetails();
+bethany.PerformWork();
+bethany.PerformWork(12);
+bethany.PerformWork();
+// bethany.ReceiveWage();
+
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30);
+
+george.DisplayEmployeeDetails();
+george.PerformWork();
+george.PerformWork();
+george.PerformWork(3);
+george.PerformWork();
+george.PerformWork(8);
+
+var receivedWageGeorge = george.ReceiveWage(ref employee, true);
