4556bb86bee9bcc98f2e3a3ba119b79b1002da94
Konradovsky <konrad.trade@gmail.com>
2025-03-07 10:35:19 +0100

    feat: my first swagger ui for asp.net

--- ASP.NET Core Web Api/API/Program.cs
+++ ASP.NET Core Web Api/API/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -13,6 +14,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 var summaries = new[]
 {
