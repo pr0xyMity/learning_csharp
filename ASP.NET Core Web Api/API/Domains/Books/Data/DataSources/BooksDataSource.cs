513f52bb8fc91f4a6c95b13d042c4b3be6c08708
Konradovsky <konrad.trade@gmail.com>
2025-03-11 12:43:42 +0100

    feat: update datasource for books

--- ASP.NET Core Web Api/API/Domains/Books/Data/DataSources/BooksDataSource.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Data/DataSources/BooksDataSource.cs
@@ -8,42 +8,33 @@ public interface IBooksDatasource
    /// <summary>
    /// Get single book details with the book ID.
    /// </summary>
-   Task<BookDTO?> getBook(Guid bookId);
+   Task<Book?> GetBook(string bookId);
    
    /// <summary>
    /// Get whole list of available books.
    /// </summary>
-   Task<List<BookDTO?>> getBooks();
+   Task<List<Book?>> GetBooks();
 }
 
 public class BooksDataSource: IBooksDatasource
 {
    
-   public async Task<BookDTO?> getBook(Guid bookId)
+   public async Task<Book?> GetBook(string bookId)
    {
       await Task.Delay(1);
       return
-         new BookDTO(
-            "How I learn",
-            new Author("Bob", "Fischer", "bob.fischer@gmail.com"),
-            null
-         );
+         new Book( "How I learn" );
    }
 
-   public async Task<List<BookDTO?>> getBooks()
+   public async Task<List<Book?>> GetBooks()
    {
       await Task.Delay(1);
-      return new List<BookDTO>([
-            new BookDTO(
-               "How I learn",
-               new Author("Bob", "Fischer", "bob.fischer@gmail.com"),
-               null
-            ),
-            new BookDTO(
-               "How I sleep",
-               new Author("Martin", "Robert", "martin.robert@gmail.com"),
-               null
-            )]
+      return new List<Book>([
+         new Book( "How I learn" ),
+         new Book( "How I sleep" ),
+         new Book( "How I design" ),
+         new Book( "How I think" ),
+            ]
       )!;
    }
 }
