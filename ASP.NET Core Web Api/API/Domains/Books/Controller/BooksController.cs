2f5ddf29fe96c8eb6d75daad7732bb1e6d240ec8
Konradovsky <konrad.trade@gmail.com>
2025-03-11 09:54:55 +0100

    feat: add mail service to getBooks endpoint for manual tests

--- ASP.NET Core Web Api/API/Domains/Books/Controller/BooksController.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Controller/BooksController.cs
@@ -1,5 +1,6 @@
 using API.Domains.Books.Domain;
 using API.Domains.Books.Domain.Repositories;
+using API.Domains.Mail.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Domains.Books.Controller;
@@ -9,16 +10,21 @@ namespace API.Domains.Books.Controller;
 public class BooksController : ControllerBase
 {
    private readonly IBooksRepository _booksRepository;
+   private readonly IMailService _mailService;
 
-   public BooksController(IBooksRepository booksRepository)
+   public BooksController(IBooksRepository booksRepository, IMailService mailService)
    {
        this._booksRepository = booksRepository ?? throw new ArgumentNullException(nameof(booksRepository));
+       this._mailService = mailService ?? throw new ArgumentNullException(nameof(mailService));
+       
    }
 
    [HttpGet]
    [ProducesResponseType(typeof(List<BookDTO>), StatusCodes.Status200OK)]
    public async Task<ActionResult<List<BookDTO>>> GetBooks()
    {
+       _mailService.Send("GetBooks", "Took the books!");
+       
        List<BookDTO?> books = await _booksRepository.getBooks();
        if (books.Count() == 0)
        {
