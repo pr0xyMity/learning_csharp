26d6e1e8a8fe93c441e68cc5ed40f988909173cb
Konradovsky <konrad.trade@gmail.com>
2025-03-12 16:13:08 +0100

    feat: working db connection

--- ASP.NET Core Web Api/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ ASP.NET Core Web Api/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a560f5f9969a3fc775d082007cfff56a7fd59f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0155828c381df5549d998e6468c336f5473e76b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
