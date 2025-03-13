e68ecfedde60987f929de4e3d26b80a5e216f83a
Konradovsky <konrad.trade@gmail.com>
2025-03-13 15:57:12 +0100

    feat: extend the books datasource and add db queries

--- ASP.NET Core Web Api/API/Domains/Books/Data/DataSources/BooksDataSource.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Data/DataSources/BooksDataSource.cs
@@ -1,4 +1,5 @@
 using API.Domains.Books.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Domains.Books.Data.DataSources;
 
@@ -13,54 +14,44 @@ public interface IBooksDatasource
     ///     Get whole list of available books.
     /// </summary>
     Task<List<Book>> GetBooks();
+
+    /// <summary>
+    ///     Get single author details with the book ID.
+    /// </summary>
+    Task<Author?> GetAuthor(string authorId);
+
+    /// <summary>
+    ///     Get whole list of available authors.
+    /// </summary>
+    Task<List<Author>> GetAuthors();
 }
 
 public class BooksDataSource : IBooksDatasource
 {
+    private readonly BookContext _bookContext;
+
+    public BooksDataSource(BookContext bookContext)
+    {
+        _bookContext = bookContext ?? throw new ArgumentNullException(nameof(bookContext));
+    }
+
     public async Task<Book?> GetBook(string bookId)
     {
-        await Task.Delay(1);
-        return
-            new Book
-            {
-                Id = bookId,
-                Authors =
-                [
-                    new Author
-                    {
-                        Name = "Tony Montana"
-                    }
-                ]
-            };
+        return await _bookContext.Books.Where(book => book.Id == bookId).FirstOrDefaultAsync();
     }
 
     public async Task<List<Book>> GetBooks()
     {
-        await Task.Delay(1);
-        return
-        [
-            new Book
-            {
-                Id = "1",
-                Authors =
-                [
-                    new Author
-                    {
-                        Name = "Tony Montana"
-                    }
-                ]
-            },
-            new Book
-            {
-                Id = "2",
-                Authors =
-                [
-                    new Author
-                    {
-                        Name = "Marko Toro"
-                    }
-                ]
-            }
-        ];
+        return await _bookContext.Books.ToListAsync();
+    }
+
+    public async Task<Author?> GetAuthor(string authorId)
+    {
+        return await _bookContext.Authors.Where(author => author.Id == authorId).FirstOrDefaultAsync();
+    }
+
+    public async Task<List<Author>> GetAuthors()
+    {
+        return await _bookContext.Authors.ToListAsync();
     }
 }
