dc8db8b6a6fbff5007f4a3871f855ef6367cadca
Konradovsky <konrad.trade@gmail.com>
2025-03-13 15:58:16 +0100

    feat: extended books controller with mapper

--- ASP.NET Core Web Api/API/Domains/Books/Controller/BooksController.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Controller/BooksController.cs
@@ -1,7 +1,7 @@
 using API.Domains.Books.Domain;
-using API.Domains.Books.Domain.Models;
 using API.Domains.Books.Domain.Repositories;
 using API.Domains.Mail.Domain.Services;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Domains.Books.Controller;
@@ -10,44 +10,37 @@ namespace API.Domains.Books.Controller;
 [Route("api/books")]
 public class BooksController : ControllerBase
 {
-   private readonly IBooksRepository _booksRepository;
-   private readonly IMailService _mailService;
+    private readonly IBooksRepository _booksRepository;
+    private readonly IMailService _mailService;
+    private readonly IMapper _mapper;
 
-   public BooksController(IBooksRepository booksRepository, IMailService mailService)
-   {
-       this._booksRepository = booksRepository ?? throw new ArgumentNullException(nameof(booksRepository));
-       this._mailService = mailService ?? throw new ArgumentNullException(nameof(mailService));
-       
-   }
+    public BooksController(IBooksRepository booksRepository, IMailService mailService, IMapper mapper)
+    {
+        _booksRepository = booksRepository ?? throw new ArgumentNullException(nameof(booksRepository));
+        _mailService = mailService ?? throw new ArgumentNullException(nameof(mailService));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
 
-   [HttpGet]
-   [ProducesResponseType(typeof(List<BookDTO>), StatusCodes.Status200OK)]
-   public async Task<ActionResult<List<BookDTO>>> GetBooks()
-   {
-       _mailService.Send("GetBooks", "Took the books!");
-       
-       List<BookModel> books = await _booksRepository.GetBooks();
-       
-       if (books.Count() == 0)
-       {
-           return NoContent();
-       }
-       return Ok(books);
-   }
-   
-   [HttpGet("{id:guid}")]
-   [ProducesResponseType(typeof(BookDTO), StatusCodes.Status200OK)]
-   public async Task<ActionResult<BookDTO>> GetBookById(string id)
-   {
-       BookModel? book = await _booksRepository.GetBookById(id);
-       
-       if (book == null)
-       {
-           return NoContent();
-       }
+    [HttpGet]
+    [ProducesResponseType(typeof(List<BookDTO>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<List<BookDTO>>> GetBooks()
+    {
+        _mailService.Send("GetBooks", "Took the books!");
 
-       BookDTO bookDto = new BookDTO(book.Id, book.Title, book.Author);
+        var books = await _booksRepository.GetBooks();
 
-       return Ok(book);
-   }
+        if (!books.Any()) return NoContent();
+        return Ok(_mapper.Map<List<BookDTO>>(books));
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(BookDTO), StatusCodes.Status200OK)]
+    public async Task<ActionResult<BookDTO>> GetBookById(string id)
+    {
+        var book = await _booksRepository.GetBookById(id);
+
+        if (book == null) return NoContent();
+
+        return Ok(_mapper.Map<BookDTO>(book));
+    }
 }
