038aa49943aca4f6971770185500597b60352eaa
Konradovsky <konrad.trade@gmail.com>
2025-03-13 15:55:06 +0100

    feat: create authorsDTO

--- ASP.NET Core Web Api/API/Domains/Books/Domain/DTO/AuthorDTO.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Domain/DTO/AuthorDTO.cs
@@ -1,12 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace API.Domains.Books.Domain;
 
 public class AuthorDTO
 {
-    public string Id { get; set; } 
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string Id { get; set; }
+    public string Name { get; set; }
     public string? Email { get; set; }
+    public ICollection<BookDTO> Books { get; set; } = new List<BookDTO>();
 }
