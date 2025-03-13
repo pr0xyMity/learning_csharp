4022ae76bb189991c45914ddd5077d41c4bf0c4d
Konradovsky <konrad.trade@gmail.com>
2025-03-13 16:10:40 +0100

    fix: bookDTO and bookModel

--- ASP.NET Core Web Api/API/Domains/Books/Domain/DTO/BookDTO.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Domain/DTO/BookDTO.cs
@@ -1,27 +1,19 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace API.Domains.Books.Domain;
 
 public class BookDTO
 {
-    public string Id { get; set; }
-    
-    public string Title  { get; set; } 
-    
-    public ICollection<Author> Author  { get; set; }
-
-    public int NumberOfAuthors
-    {
-        get
-        {
-            return Author.Count();
-        }
-    }
-
-    public BookDTO(string id, string title, ICollection<Author> author)
+    public BookDTO(string id, string title, ICollection<Author> authors)
     {
         Id = id;
         Title = title;
-        Author = author;
+        Authors = authors;
     }
+
+    public string Id { get; set; }
+
+    public string Title { get; set; }
+
+    public ICollection<Author> Authors { get; set; }
+
+    public int NumberOfAuthors => Authors.Count();
 }
--- ASP.NET Core Web Api/API/Domains/Books/Domain/Models/BookModel.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Domain/Models/BookModel.cs
@@ -2,33 +2,31 @@ namespace API.Domains.Books.Domain.Models;
 
 public class BookModel
 {
-    public string Id { get; set; }
-    
-    public string Title  { get; set; } 
-    
-    public ICollection<Author> Author  { get; set; }
-
-    public BookModel(string id, string title, ICollection<Author> author)
+    public BookModel(string id, string title, ICollection<Author> authors)
     {
         Id = id;
         Title = title;
-        Author = author;
+        Authors = authors;
     }
-    
-   public bool IsBookEbook()
-   {
-       return Id.Contains("EB");
-   }
-   
-   public bool IsBookAvailable()
-   {
-       return !Title.Contains("Available");
-   }
-   
-   public bool IsWrittenBySingeAuthor()
-   {
-       return Author.Count > 1;
-   }
-   
-   
+
+    public string Id { get; set; }
+
+    public string Title { get; set; }
+
+    public ICollection<Author> Authors { get; set; }
+
+    public bool IsBookEbook()
+    {
+        return Id.Contains("EB");
+    }
+
+    public bool IsBookAvailable()
+    {
+        return !Title.Contains("Available");
+    }
+
+    public bool IsWrittenBySingeAuthor()
+    {
+        return Authors.Count > 1;
+    }
 }
