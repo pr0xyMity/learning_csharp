a946e50db283bbb67c0ee32d192060df0cad6c34
Konradovsky <konrad.trade@gmail.com>
2025-03-13 15:56:09 +0100

    feat: create authors profile

---
+++ ASP.NET Core Web Api/API/Domains/Books/Domain/Profiles/AuthorsProfile.cs
@@ -0,0 +1,13 @@
+using API.Domains.Books.Domain.Models;
+using AutoMapper;
+
+namespace API.Domains.Books.Domain.Profiles;
+
+public class AuthorsProfile : Profile
+{
+    public AuthorsProfile()
+    {
+        CreateMap<Author, AuthorModel>();
+        CreateMap<AuthorModel, AuthorDTO>();
+    }
+}
