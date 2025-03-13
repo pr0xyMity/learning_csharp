b0411f99dd560cc1499ed6a96c5ca740223ace98
Konradovsky <konrad.trade@gmail.com>
2025-03-13 10:12:56 +0100

    feat: added many-to-many relation and a seeder

--- ASP.NET Core Web Api/API/Domains/Core/DbContexts/BookInfoContext.cs
+++ ASP.NET Core Web Api/API/Domains/Core/DbContexts/BookInfoContext.cs
@@ -2,13 +2,63 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Domains.Books.Domain;
 
-public class BookInfoContext: DbContext
-{ 
-	public DbSet<Book> Books { get; set; }
-	public DbSet<Author>  Authors { get; set; }
-	
-	public BookInfoContext(DbContextOptions<BookInfoContext> options)
-		: base(options)
-	{
-	}
+public class BookInfoContext : DbContext
+{
+    public BookInfoContext(DbContextOptions<BookInfoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>()
+            .HasMany<Book>(a => a.Books)
+            .WithMany(b => b.Authors)
+            .UsingEntity(j => j.ToTable("AuthorBook"));
+
+        modelBuilder.Entity<Book>()
+            .HasMany<Author>(b => b.Authors)
+            .WithMany(a => a.Books)
+            .UsingEntity(j => j.ToTable("AuthorBook"));
+
+        // Ensure that a book has at least one author
+        // modelBuilder.Entity<Book>()
+        //     .HasCheckConstraint("CK_Book_AtLeastOneAuthor", "EXISTS (SELECT 1 FROM AuthorBook WHERE BookId = Id)");
+
+        SeedData(modelBuilder);
+    }
+
+    private void SeedData(ModelBuilder modelBuilder)
+    {
+        var authors = new List<Author>
+        {
+            new() { Id = "1", Name = "George Orwell" },
+            new() { Id = "2", Name = "J.K. Rowling" },
+            new() { Id = "3", Name = "J.R.R. Tolkien" }
+        };
+
+        var books = new List<Book>
+        {
+            new() { Id = "1", Title = "1984" },
+            new() { Id = "2", Title = "Animal Farm" },
+            new() { Id = "3", Title = "Harry Potter and the Philosopher's Stone" },
+            new() { Id = "4", Title = "The Lord of the Rings" },
+            new() { Id = "5", Title = "The Hobbit" }
+        };
+
+        modelBuilder.Entity<Author>().HasData(authors);
+        modelBuilder.Entity<Book>().HasData(books);
+
+        // Seed the many-to-many relationship
+        modelBuilder.Entity("AuthorBook").HasData(
+            new { AuthorsId = "1", BooksId = "1" },
+            new { AuthorsId = "1", BooksId = "2" },
+            new { AuthorsId = "2", BooksId = "3" },
+            new { AuthorsId = "3", BooksId = "4" },
+            new { AuthorsId = "3", BooksId = "5" }
+        );
+    }
 }
