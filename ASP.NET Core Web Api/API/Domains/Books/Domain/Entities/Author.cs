46e1d29661dda3e261b9e6370454bdef3ec731ec
Konradovsky <konrad.trade@gmail.com>
2025-03-13 10:12:22 +0100

    fix: author entity changed, removed constructor and added many-to-many relation

--- ASP.NET Core Web Api/API/Domains/Books/Domain/Entities/Author.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Domain/Entities/Author.cs
@@ -5,29 +5,11 @@ namespace API.Domains.Books.Domain;
 
 public class Author
 {
-    [Key] 
+    [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public string Id  { get; set; } 
-    
-    [Required] 
-    [MaxLength(50)]
-    public string FirstName { get; set; }
-    
-    [Required] 
-    [MaxLength(50)]
-    public string LastName { get; set; }
-    
-    [MaxLength(60)]
-    [EmailAddress]
-    public string? Email { get; set; }
-    
-    [ForeignKey("BookId")]
-    public Book? Book { get; set; }
-    public string? CityId { get; set; }
+    public string Id { get; set; }
 
-    public Author(string firstName, string lastName)
-    {
-        FirstName = firstName;
-        LastName = lastName;
-    }
+    [Required] [MaxLength(50)] public string Name { get; set; }
+    [MaxLength(60)] [EmailAddress] public string? Email { get; set; }
+    [Required] public List<Book> Books { get; set; } = new();
 }
