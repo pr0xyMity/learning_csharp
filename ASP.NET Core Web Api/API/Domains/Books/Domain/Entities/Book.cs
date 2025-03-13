f9695c2d9eeb9677bcba605ef5c122b75c2ea9b4
Konradovsky <konrad.trade@gmail.com>
2025-03-13 10:12:00 +0100

    fix: book entity changed, removed constructor

--- ASP.NET Core Web Api/API/Domains/Books/Domain/Entities/Book.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Domain/Entities/Book.cs
@@ -5,32 +5,15 @@ namespace API.Domains.Books.Domain;
 
 public class Book
 {
-    [Key] 
+    [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string Id { get; set; }
-    
-    [Required] 
-    [MaxLength(100)]
-    public string Title { get; set; }
-    
-    [Required] 
-    [MinLength(1)]
-    public ICollection<Author> Authors { get; set; } = new List<Author>();
-    
+
+    [Required] [MaxLength(100)] public string Title { get; set; }
+
     [Required]
-    [MaxLength(400)]
-    public string Description { get; set; }
+    [MinLength(1, ErrorMessage = "Book must at least have one author.")]
+    public List<Author> Authors { get; set; } = new();
 
-    // Private parameterless constructor for EF Core
-    private Book() {}
-
-    // Public constructor for domain logic
-    public Book(string title, ICollection<Author> authors)
-    {
-        Title = title;
-        Authors = authors ?? throw new ArgumentNullException(nameof(authors));
-        
-        if (authors.Count == 0)
-            throw new ArgumentException("At least one author is required", nameof(authors));
-    }
+    [MaxLength(400)] public string? Description { get; set; }
 }
