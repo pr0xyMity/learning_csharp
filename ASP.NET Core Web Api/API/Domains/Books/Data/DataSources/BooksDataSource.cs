661edb70bf49e378a409d8124b939b537e8905af
Konradovsky <konrad.trade@gmail.com>
2025-03-13 10:13:27 +0100

    fix: books datasource to accept the new entity

--- ASP.NET Core Web Api/API/Domains/Books/Data/DataSources/BooksDataSource.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Data/DataSources/BooksDataSource.cs
@@ -1,51 +1,66 @@
 using API.Domains.Books.Domain;
-using Microsoft.AspNetCore.Mvc;
 
 namespace API.Domains.Books.Data.DataSources;
 
 public interface IBooksDatasource
 {
-   /// <summary>
-   /// Get single book details with the book ID.
-   /// </summary>
-   Task<Book?> GetBook(string bookId);
-   
-   /// <summary>
-   /// Get whole list of available books.
-   /// </summary>
-   Task<List<Book>> GetBooks();
+    /// <summary>
+    ///     Get single book details with the book ID.
+    /// </summary>
+    Task<Book?> GetBook(string bookId);
+
+    /// <summary>
+    ///     Get whole list of available books.
+    /// </summary>
+    Task<List<Book>> GetBooks();
 }
 
-public class BooksDataSource: IBooksDatasource
+public class BooksDataSource : IBooksDatasource
 {
-   
-   public async Task<Book?> GetBook(string bookId)
-   {
-      await Task.Delay(1);
-      return
-         new Book( "How I learn" ,
-            new List<Author>([new Author("Tony", "Smart")]
-               )
-            );
-   }
+    public async Task<Book?> GetBook(string bookId)
+    {
+        await Task.Delay(1);
+        return
+            new Book
+            {
+                Id = bookId,
+                Authors =
+                [
+                    new Author
+                    {
+                        Name = "Tony Montana"
+                    }
+                ]
+            };
+    }
 
-   public async Task<List<Book>> GetBooks()
-   {
-      await Task.Delay(1);
-      return new List<Book>([
-         new Book( "How I learn" ,
-            new List<Author>(
-               [new Author("Tony", "Smart")]
-            )),
-         new Book( "How I sleep" ,
-            new List<Author>([new Author("Amorio", "Snow")]
-            )),
-         new Book( "How I design" ,
-            new List<Author>([new Author("Fito", "Kamino")]
-            )),
-         new Book( "How I think" ,
-            new List<Author>([new Author("Marco", "Look")])
-            ),
-      ])!;
-   }
+    public async Task<List<Book>> GetBooks()
+    {
+        await Task.Delay(1);
+        return
+        [
+            new Book
+            {
+                Id = "1",
+                Authors =
+                [
+                    new Author
+                    {
+                        Name = "Tony Montana"
+                    }
+                ]
+            },
+            new Book
+            {
+                Id = "2",
+                Authors =
+                [
+                    new Author
+                    {
+                        Name = "Marko Toro"
+                    }
+                ]
+            }
+        ];
+    }
 }
