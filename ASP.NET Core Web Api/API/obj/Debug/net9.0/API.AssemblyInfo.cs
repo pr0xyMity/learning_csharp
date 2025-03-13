2950f3566421d18b54a6011aabf2967a4e448134
Konradovsky <konrad.trade@gmail.com>
2025-03-13 15:58:29 +0100

    feat: installed auto mapper

--- ASP.NET Core Web Api/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ ASP.NET Core Web Api/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77fa428f2703179709837044b69317b3353d899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0050b8f7801a5b7250aa6adb8faec52bf3d0d6f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
