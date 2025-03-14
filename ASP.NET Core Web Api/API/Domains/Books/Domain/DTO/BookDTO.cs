385565a1adb1af548c7989ba5741f21c67fc24b1
Konradovsky <konrad.trade@gmail.com>
2025-03-14 11:01:30 +0100

    fix: show the authors without books to not run into circular deps

--- ASP.NET Core Web Api/API/Domains/Books/Domain/DTO/BookDTO.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Domain/DTO/BookDTO.cs
@@ -2,7 +2,7 @@ namespace API.Domains.Books.Domain;
 
 public class BookDTO
 {
-    public BookDTO(string id, string title, ICollection<Author> authors)
+    public BookDTO(string id, string title, ICollection<AuthorWithoutBooksDTO> authors)
     {
         Id = id;
         Title = title;
@@ -13,7 +13,7 @@ public class BookDTO
 
     public string Title { get; set; }
 
-    public ICollection<Author> Authors { get; set; }
+    public ICollection<AuthorWithoutBooksDTO> Authors { get; set; }
 
     public int NumberOfAuthors => Authors.Count();
 }
