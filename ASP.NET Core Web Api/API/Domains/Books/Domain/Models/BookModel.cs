821fb47e21cd12f8d11f699b84375d959bb5f31c
Konradovsky <konrad.trade@gmail.com>
2025-03-14 10:59:27 +0100

    fix: same type for Authors in Model

--- ASP.NET Core Web Api/API/Domains/Books/Domain/Models/BookModel.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Domain/Models/BookModel.cs
@@ -2,18 +2,11 @@ namespace API.Domains.Books.Domain.Models;
 
 public class BookModel
 {
-    public BookModel(string id, string title, ICollection<Author> authors)
-    {
-        Id = id;
-        Title = title;
-        Authors = authors;
-    }
-
     public string Id { get; set; }
 
     public string Title { get; set; }
 
-    public ICollection<Author> Authors { get; set; }
+    public List<AuthorModel> Authors { get; set; } = new();
 
     public bool IsBookEbook()
     {
