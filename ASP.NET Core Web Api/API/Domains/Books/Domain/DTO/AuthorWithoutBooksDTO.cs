ace371132387bffb2a13d82ab02d9d79b8061763
Konradovsky <konrad.trade@gmail.com>
2025-03-17 09:47:27 +0100

    fix: remove unused author without books

--- ASP.NET Core Web Api/API/Domains/Books/Domain/DTO/AuthorWithoutBooksDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace API.Domains.Books.Domain;
-
-public class AuthorWithoutBooksDTO
-{
-    public required string Id { get; set; }
-    public required string Name { get; set; }
-    public string? Email { get; set; }
-}
