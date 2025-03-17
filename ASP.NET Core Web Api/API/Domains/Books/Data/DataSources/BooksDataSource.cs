3e52523fc0f83eb63395ce92407a73f2822f519d
Konradovsky <konrad.trade@gmail.com>
2025-03-17 09:51:18 +0100

    fix: split authors to a separate data source

--- ASP.NET Core Web Api/API/Domains/Books/Data/DataSources/BooksDataSource.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Data/DataSources/BooksDataSource.cs
@@ -15,16 +15,6 @@ public interface IBooksDatasource
     ///     Get whole list of available books.
     /// </summary>
     Task<List<BookModel>> GetBooks();
-
-    /// <summary>
-    ///     Get single author details with the book ID.
-    /// </summary>
-    Task<Author?> GetAuthor(string authorId);
-
-    /// <summary>
-    ///     Get whole list of available authors.
-    /// </summary>
-    Task<List<Author>> GetAuthors();
 }
 
 public class BooksDataSource : IBooksDatasource
@@ -56,15 +46,5 @@ public class BooksDataSource : IBooksDatasource
                 }).ToList()
             })
             .ToListAsync();
-    }
-
-    public async Task<Author?> GetAuthor(string authorId)
-    {
-        return await _bookContext.Authors.Where(author => author.Id == authorId).FirstOrDefaultAsync();
-    }
-
-    public async Task<List<Author>> GetAuthors()
-    {
-        return await _bookContext.Authors.ToListAsync();
     }
 }
