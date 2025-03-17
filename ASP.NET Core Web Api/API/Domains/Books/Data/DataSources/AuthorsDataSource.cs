af05b4b09ddb47914654b09ae4a94a83616768b3
Konradovsky <konrad.trade@gmail.com>
2025-03-17 09:48:52 +0100

    feat: created authors datasource

---
+++ ASP.NET Core Web Api/API/Domains/Books/Data/DataSources/AuthorsDataSource.cs
@@ -0,0 +1,67 @@
+using API.Domains.Books.Domain;
+using API.Domains.Books.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Domains.Books.Data.DataSources;
+
+public interface IAuthorsDatasource
+{
+    /// <summary>
+    ///     Get single author details with the books IDs.
+    /// </summary>
+    Task<Author?> GetAuthor(string authorId);
+
+    /// <summary>
+    ///     Get whole list of available authors with book ID and title.
+    /// </summary>
+    Task<List<AuthorModel>> GetAuthorsWithBooks();
+
+    /// <summary>
+    ///     Get whole list of available authors without book IDs.
+    /// </summary>
+    Task<List<AuthorModel>> GetAuthorsWithoutBooks();
+}
+
+public class AuthorsDataSource : IAuthorsDatasource
+{
+    private readonly BookContext _bookContext;
+
+    public AuthorsDataSource(BookContext bookContext)
+    {
+        _bookContext = bookContext ?? throw new ArgumentNullException(nameof(bookContext));
+    }
+
+    public async Task<Author?> GetAuthor(string authorId)
+    {
+        return await _bookContext.Authors.Where(author => author.Id == authorId).FirstOrDefaultAsync();
+    }
+
+    public async Task<List<AuthorModel>> GetAuthorsWithoutBooks()
+    {
+        return await _bookContext.Authors
+            .Select(author => new AuthorModel
+            {
+                Id = author.Id,
+                Name = author.Name,
+                Books = new List<BookModelWithoutAuthors>()
+            })
+            .ToListAsync();
+    }
+
+    public async Task<List<AuthorModel>> GetAuthorsWithBooks()
+    {
+        return await _bookContext.Authors
+            .Include(author => author.Books)
+            .Select(author => new AuthorModel
+            {
+                Id = author.Id,
+                Name = author.Name,
+                Books = author.Books.Select(book => new BookModelWithoutAuthors
+                {
+                    Id = book.Id,
+                    Title = book.Title
+                }).ToList()
+            })
+            .ToListAsync();
+    }
+}
