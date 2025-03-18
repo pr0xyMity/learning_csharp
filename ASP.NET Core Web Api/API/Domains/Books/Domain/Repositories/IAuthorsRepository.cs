945443a76923f7a5a5ce5c86920c2cb56f106dd6
Konradovsky <konrad.trade@gmail.com>
2025-03-18 13:44:49 +0100

    feat: GetAuthorsByIds repository interface update

--- ASP.NET Core Web Api/API/Domains/Books/Domain/Repositories/IAuthorsRepository.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Domain/Repositories/IAuthorsRepository.cs
@@ -7,5 +7,7 @@ public interface IAuthorsRepository
     Task<AuthorModel> AddAuthor(AuthorForCreationDto authorDto);
     Task RemoveAuthor(string authorId);
     Task<List<AuthorModel>> GetAuthors(bool includeBooks);
+
+    Task<List<AuthorModel>> GetAuthorsByIds(List<string> authorIds);
     Task<AuthorModel?> GetAuthor(string authorId);
 }
