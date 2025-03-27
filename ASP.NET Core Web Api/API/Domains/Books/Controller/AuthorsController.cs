11ba2f8e4d5b1b510860befe594a5531e97f8fe8
Konradovsky <konrad.trade@gmail.com>
2025-03-27 15:58:14 +0100

    feat: bring back the post for check

--- ASP.NET Core Web Api/API/Domains/Books/Controller/AuthorsController.cs
+++ ASP.NET Core Web Api/API/Domains/Books/Controller/AuthorsController.cs
@@ -39,16 +39,16 @@ public class AuthorsController : ControllerBase
         return NoContent();
     }
 
-    // [HttpPost]
-    // [Route("/api/authors/{authorId}/books")]
-    // [ProducesResponseType(typeof(AuthorDto), StatusCodes.Status201Created)]
-    // public async Task<ActionResult<AuthorDto>> AddBooksToAuthor(AuthorForCreationDto authorForCreationDto)
-    // {
-    //     _mailService.Send("CreateAuthors", "You want to write huh?");
-    //     var authorToReturn = await _authorsRepository.AddAuthor(authorForCreationDto);
-    //     var uri = Url.Action(nameof(GetAuthorById), new { id = authorToReturn.Id });
-    //     return Created(uri, authorToReturn);
-    // }
+    [HttpPost]
+    [Route("/api/authors/{authorId}/books")]
+    [ProducesResponseType(typeof(AuthorDto), StatusCodes.Status201Created)]
+    public async Task<ActionResult<AuthorDto>> AddBooksToAuthor(AuthorForCreationDto authorForCreationDto)
+    {
+        _mailService.Send("CreateAuthors", "You want to write huh?");
+        var authorToReturn = await _authorsRepository.AddAuthor(authorForCreationDto);
+        var uri = Url.Action(nameof(GetAuthorById), new { id = authorToReturn.Id });
+        return Created(uri, authorToReturn);
+    }
 
     [HttpPut("{authorId}")]
     [ProducesResponseType(typeof(AuthorDto), StatusCodes.Status200OK)]
