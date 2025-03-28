6ef7fd797e9a07ef2cb5d092599775e6fa4a6cb8
Konradovsky <konrad.trade@gmail.com>
2025-03-28 12:22:25 +0100

    feat: extend IAuthenticationService.cs

--- ASP.NET Core Web Api/API/Domains/Authentication/Domain/Services/IAuthenticationService.cs
+++ ASP.NET Core Web Api/API/Domains/Authentication/Domain/Services/IAuthenticationService.cs
@@ -1,3 +1,4 @@
+using API.Domains.Authentication.Domain.Dto;
 using API.Domains.Books;
 
 namespace API.Domains.Authentication.Domain.Services;
@@ -5,5 +6,7 @@ namespace API.Domains.Authentication.Domain.Services;
 public interface IAuthenticationService
 {
     string CreateToken(User user);
-    User? ValidateUserCredentials(string? username, string? password);
+    User? ValidateUserCredentials(AuthenticationRequestBodyDto authenticationRequestBodyDto);
+    Task<User> CreateUser(RegisterRequestBodyDto registerRequestBodyDto);
+    Task<bool> EmailExists(string email);
 }
