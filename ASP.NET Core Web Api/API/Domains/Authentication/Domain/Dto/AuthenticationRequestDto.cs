e9ad8bd9aef088a9889c2dc8f8550e1b83706952
Konradovsky <konrad.trade@gmail.com>
2025-03-28 12:23:06 +0100

    feat: extend AuthenticationController with email

--- ASP.NET Core Web Api/API/Domains/Authentication/Domain/Dto/AuthenticationRequestDto.cs
+++ ASP.NET Core Web Api/API/Domains/Authentication/Domain/Dto/AuthenticationRequestDto.cs
@@ -2,6 +2,7 @@ namespace API.Domains.Authentication.Domain.Dto;
 
 public abstract class AuthenticationRequestDto
 {
+    public string? Email { get; set; }
     public string? Username { get; set; }
     public string? Password { get; set; }
 }
